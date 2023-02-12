cabaeac1d4186ab33c0bba8e835ec5861ed6ed18
sergenast <nastia0709@mail.ru>
2023-02-12 16:54:44 +0300

    added index.js and readme

---
+++ index.js
@@ -0,0 +1,36 @@
+let workman = 40000,
+    singer = 22000;
+console.log(workman + singer);
+
+let x = "Владивосток ",
+    y = 2000;
+console.log(x + y);
+
+let z = 128,
+    u = 254,
+    w = 137,
+    q = 201;
+console.log((z + u + w + q) / 60);
+
+console.log(2 ** 5);
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет";
+const name = "Кот";
+const r = `, `
+alert(greetings + `${r}` + name);
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = time - 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = time - 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+let fahrenheit = 451
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + " градусов по Фаренгейту - это " + celsius + " градусов по Цельсию");
